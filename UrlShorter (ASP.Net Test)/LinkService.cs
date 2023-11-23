f0dac24cb497e72057ab5b01f40aac0203e65d75
Saveliy Savenak <1986developer@gmail.com>
2023-11-23 16:27:26 +0300

    Added tests

---
+++ UrlShorter (ASP.Net Test)/LinkService.cs
@@ -0,0 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using UrlShorter.Database;
+using UrlShorter.Services.Link.LinkModels;
+
+namespace UrlShorter.Tests
+{
+    [TestClass]
+    public class LinkService
+    {
+        const string connectionstring = "Server=localhost; Database=urlshortertest; Uid=aspnet; Password=Qwerty123"; //Change it for your DB
+        DatabaseService database = new DatabaseService(new DbContextOptionsBuilder<DatabaseService>().UseMySQL(connectionstring).Options);
+        public void resetDB()
+        {
+            database.Database.EnsureDeleted();
+            database.Database.EnsureCreated();
+        }
+        [TestMethod]
+        public async Task AddLink()
+        {
+            resetDB();
+            var service = new Services.Link.LinkService(database);
+            var link = await service.CreateLink("https://google.com");
+            var returned = await service.GetLinkById(link.Id);
+            Assert.AreEqual<Link?>(link, returned);
+        }
+        [TestMethod]
+        public async Task NotExistingLink()
+        {
+            resetDB();
+            var service = new Services.Link.LinkService(database);;
+            var returned = await service.GetLinkById(1);
+            Assert.AreEqual(returned, null);
+        }
+        [TestMethod]
+        public async Task RemoveLink()
+        {
+            resetDB();
+            var service = new Services.Link.LinkService(database);
+            var link = await service.CreateLink("https://google.com");
+            await service.RemoveLinkById(link.Id);
+            var returned = await service.GetLinkById(link.Id);
+            Assert.AreEqual(returned, null);
+        }
+        [TestMethod]
+        public async Task ModifiedLink()
+        {
+            resetDB();
+            var service = new Services.Link.LinkService(database);
+            var link = await service.CreateLink("https://google.com");
+            await service.ModifyLink(link.Id, "https://vk.com");
+            var returned = await service.GetLinkById(link.Id);
+            Assert.AreNotEqual(link.DestinationUrl, returned!.DestinationUrl);
+        }
+    }
+}
