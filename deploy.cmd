f94f9943ed259477caeff43503725152a17c2e72
Saveliy Savenak <1986developer@gmail.com>
2023-11-24 22:09:00 +0300

    Changing scripts...

--- deploy.cmd
+++ deploy.cmd
@@ -1,10 +1,11 @@
+@echo off
 echo Запуск развёртывания...
 cd ASP.Net
 del /F /s /q wwwroot
 mkdir wwwroot
 cd ../Front
-yarn
-yarn build
+call yarn
+call yarn build
 cd ../ASP.Net
 dotnet publish
 echo Развёртывание завершено!
