cbd542cf0dd6d8cc2e1f9d9bb56b12a8a5037c8d
Saveliy Savenak <1986developer@gmail.com>
2023-11-24 19:04:42 +0300

    deploy to personal server

--- Front/src/configuration.ts
+++ Front/src/configuration.ts
@@ -1,7 +1,7 @@
-export class Configuration{
-    static BackEndpoint = "http://localhost:5000"
-}
+//export class Configuration{
+//    static BackEndpoint = "http://localhost:5000"
+//}
 
-// export class Configuration{
-//     static BackEndpoint = "http://url.lisoveliy.site"
-// }
+export class Configuration{
+    static BackEndpoint = "http://url.lisoveliy.site"
+}
