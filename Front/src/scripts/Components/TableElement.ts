1b5a0e65981a4d8eece71a3049466d0f2de3609d
Saveliy Savenak <1986developer@gmail.com>
2023-11-24 15:52:48 +0300

    Added edit page, small refactor

--- Front/src/scripts/Components/TableElement.ts
+++ Front/src/scripts/Components/TableElement.ts
@@ -1,25 +1,34 @@
 import { IElement } from "./IElement"
 
 export class TableElement implements IElement{
-  public constructor(public extUrl: string, public intUrl: string, public timeofcreation: string, public counter: number, table: HTMLTableElement){
+  public constructor(id: number, public extUrl: string, public intUrl: string, public timeofcreation: string, public counter: number, table: HTMLTableElement){
     const mainel = table.insertRow();
 
     const extEl = mainel.insertCell(),
     intEl = mainel.insertCell(),
     timeofcreationEl = mainel.insertCell(),
     counterEl = mainel.insertCell()
-    
+    //buttonEl = mainel.insertCell()
     extEl.append(this.generateLink(extUrl, extUrl))
     intEl.append(this.generateLink(intUrl, intUrl))
     timeofcreationEl.append(this.generateElement('p', timeofcreation))
-    counterEl.append(this.generateElement('p', String(counter)))
+    let button = document.createElement('a')
+    button.className = "btn btn-primary"
+    button.href = "edit?id=" + String(id)
+    button.role = "button"
+    button.textContent = "Изменить"
+    let counterdiv = this.generateElement('div', '<p>' + String(counter) +'</p>')
+    counterdiv.className = "flex"
+    counterdiv.append(button)
+    counterEl.append(counterdiv)
+
     this.mainElement = mainel
   }
   mainElement: HTMLElement
 
   private generateElement(tag: string, content?: string): HTMLElement {
     let mainel = document.createElement(tag)
-    mainel.textContent = content ?? ""
+    mainel.innerHTML = content ?? ""
     return mainel
   }
   private generateLink(href: string, content?: string): HTMLAnchorElement {
