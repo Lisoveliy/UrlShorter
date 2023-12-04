730c9ffeb68155cbf98d0af8ed055f5da0226f78
Saveliy Savenak <1986developer@gmail.com>
2023-12-04 11:19:13 +0300

    FIX: front double requests.

--- ASP.Net/wwwroot/scripts/createPage.js
+++ ASP.Net/wwwroot/scripts/createPage.js
@@ -5,10 +5,6 @@ import { createLink } from './Client/addData.js';
 //Input Field
 let shorturl = document.getElementById('shortUrlInput');
 //Listen events of submit
-document.addEventListener('keypress', async (e) => {
-    if (e.key == "Enter")
-        await createLink(shorturl.value);
-});
 document.getElementById('createform').addEventListener('submit', async (e) => {
     e.preventDefault();
     await createLink(shorturl.value);
